a321509c1907417beb69c1b7c9eac7a6a12f5864
Antariksh Alse <74238119+Int-coder@users.noreply.github.com>
2023-01-29 16:16:47 +0530

    Button css

--- src/components/ui/Input/Input.jsx
+++ src/components/ui/Input/Input.jsx
@@ -7,11 +7,10 @@ const Input = ({ label, name, value, onChange }) => {
     <div className="input-container mx-4">
       <div className="input-label">{label}</div>
       <input
-        className="input-label"
+        className="input-label rounded rounded-2"
         value={value}
         onChange={onChange}
         name={name}
-        placeholder="Input Search & Press Enter"
       />
     </div>
   );
